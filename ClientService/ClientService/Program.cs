acf65c103a7e8e5bf1722ab0f3b53db5d7b0b1da
SvetaYu <svetlanavolga2003@gmail.com>
2023-10-02 15:40:53 +0300

    feat: create template

---
+++ ClientService/ClientService/Program.cs
@@ -0,0 +1,18 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        if (args.Length.Equals(0)) throw new Exception();
+        switch (args[0])
+        {
+            case "get":
+                //Console.Writeline(ClientComponent.Get(args[1]))
+                break;
+            case "set":
+                // ClientComponent.Set(args[1], args[2])
+                break;
+            default:
+                throw new Exception();
+        }
+    }
+}
